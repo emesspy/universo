0833b007cac472bdc89a34be86284a433d0132db
Miguel S. Pereira <emesspy@outlook.com>
2024-11-10 12:31:56 -0300

    feature: project initializing

---
+++ src/main/kotlin/paz/sincrionario/UniversoApplication.kt
@@ -0,0 +1,11 @@
+package paz.sincrionario
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UniversoApplication
+
+fun main(args: Array<String>) {
+	runApplication<UniversoApplication>(*args)
+}
