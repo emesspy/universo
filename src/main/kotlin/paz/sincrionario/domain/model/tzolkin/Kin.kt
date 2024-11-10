b821e6d7198f19a2f477d09fd9988fd2af41d972
Miguel S. Pereira <emesspy@outlook.com>
2024-11-10 18:40:43 -0300

    fix: adjusting some cases on the border of the matrix

--- src/main/kotlin/paz/sincrionario/domain/model/tzolkin/Kin.kt
+++ src/main/kotlin/paz/sincrionario/domain/model/tzolkin/Kin.kt
@@ -3,6 +3,8 @@ package paz.sincrionario.domain.model.tzolkin
 import paz.sincrionario.domain.enums.tzolkin.Seal
 import paz.sincrionario.domain.enums.tzolkin.Tone
 
+val TZOLKIN_SIZE = 260
+
 data class Kin private constructor(
     val id: Int,
     val name: String,
@@ -34,7 +36,7 @@ fun buildTzolkin(): Map<Int, Kin> {
     val kins = mutableMapOf<Int, Kin>()
     var idKin = 1
 
-    for (i in 0 until 260) {
+    for (i in 0 until TZOLKIN_SIZE) {
         val seal = Seal.entries[i % Seal.entries.size] // 20 Selos
         val tone = Tone.entries[i % Tone.entries.size] // 13 Tons
         kins.put(idKin, Kin.of(idKin, seal, tone))
@@ -46,24 +48,41 @@ fun buildTzolkin(): Map<Int, Kin> {
 
 fun calculateKinNumber(sealId: Int, toneId: Int): Int {
     val xValue = calculateX(sealId, toneId)
-    return xValue * 20 + (sealId % 20)
+    val candidate = xValue * 20 + sealId
+    return if (candidate == TZOLKIN_SIZE) TZOLKIN_SIZE else candidate % TZOLKIN_SIZE
 }
 
-// X+1 is the number of the column the Kin is located within Tzolkin matrix
-private fun calculateX(sealId: Int, toneId: Int): Int {
+// X+1 is the number of the column the Kin is located within Tzolkin's matrix
+fun calculateX(sealId: Int, toneId: Int): Int {
     val toneSize = Tone.entries.size
     val mysticNumber = 7
-    val maxIterations = toneSize - 1
+    validateWithinRange(sealId, toneId)
+    val tomMod = toneId % Tone.entries.size
 
-    for (k in 0..maxIterations) {
-        val x = ((toneId + toneSize * k) - (sealId % toneSize)) / mysticNumber
-        if (((sealId % toneSize) + x * mysticNumber) % toneSize == toneId) {
+    for (k in 1..toneSize) {
+        val x = ((tomMod + toneSize * k) - (sealId % toneSize)) / mysticNumber
+        if (((sealId % toneSize) + x * mysticNumber) % toneSize == tomMod) {
             return x
         }
-        if (sealId == 20 && toneId == 13)
-            return 12
     }
     throw IllegalArgumentException("No value of x satisfies the equation for sealId: $sealId and toneId: $toneId.")
+}
+
+fun validateWithinRange(sealId: Int, toneId: Int) {
+    validateSealRange(sealId)
+    validateToneRange(toneId)
+}
+
+private fun validateSealRange(sealId: Int) {
+    if (sealId <= 0 || sealId > Seal.entries.size) {
+        throw IllegalArgumentException("$sealId not within Tzolkin's range for a Seal")
+    }
+}
+
+fun validateToneRange(toneId: Int) {
+    if (toneId <= 0 || toneId > Tone.entries.size) {
+        throw IllegalArgumentException("$toneId not within Tzolkin's range for a Tone")
+    }
 }
 
 private fun buildKinName(seal: Seal, tone: Tone): String {
@@ -76,7 +95,7 @@ private fun findGuideKinNumber(seal: Seal, tone: Tone): Int {
     return calculateKinNumber(guideSealId, tone.id)
 }
 
-private fun findAnalogKinNumber(seal: Seal, tone: Tone): Int {
+fun findAnalogKinNumber(seal: Seal, tone: Tone): Int {
     val analogSealCalcResult = 19 - (seal.id % Seal.entries.size)
     val analogSealId = if (analogSealCalcResult == 0) 20 else analogSealCalcResult
     return calculateKinNumber(analogSealId, tone.id)
@@ -88,7 +107,7 @@ private fun findAntipodeKinNumber(seal: Seal, tone: Tone): Int {
     return calculateKinNumber(antipodeSealId, tone.id)
 }
 
-private fun findHiddenKinNumber(seal: Seal, tone: Tone): Int {
+fun findHiddenKinNumber(seal: Seal, tone: Tone): Int {
     val hiddenSealId = 21 - seal.id
     val hiddenToneId = 14 - tone.id
     return calculateKinNumber(hiddenSealId, hiddenToneId)
